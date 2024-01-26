7c9993d486804fc8b16b9921cf48ccf9cd94f999
Nebojsa Enjev <enjev.nebojsa@gmail.com>
2024-01-26 15:56:23 +0100

    Done second part of lessons on Array and Loops in JavaScript.

--- 08_ArraysAndLoops/scripts/todo-list.js
+++ 08_ArraysAndLoops/scripts/todo-list.js
@@ -1,4 +1,4 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [];
 
 renderTodoList();
 
@@ -6,12 +6,20 @@ function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+
+    // Done like this bellow because we need 3 elements so that we can make grid of 3 columns
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="todoList.splice(${i},1); renderTodoList();" class="delete-todo-button">Delete</button>
+      `
+    ;
     todoListHTML += html;
   }
-
-  console.log(todoListHTML);
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
@@ -21,8 +29,10 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({name, dueDate});
 
   inputElement.value = '';
 
