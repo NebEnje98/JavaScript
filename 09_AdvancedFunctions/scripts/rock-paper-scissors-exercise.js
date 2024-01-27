fb691802b46eadc56aaee1c9921ce8713def1383
Nebojsa Enjev <enjev.nebojsa@gmail.com>
2024-01-27 13:00:43 +0100

    Done exercises for second part of Advanced Functions in JavaScript.

---
+++ 09_AdvancedFunctions/scripts/rock-paper-scissors-exercise.js
@@ -0,0 +1,164 @@
+let score = JSON.parse(localStorage.getItem('score')) || {wins : 0, losses : 0, ties : 0};
+
+updateScoreElement();
+
+let isAutoPlaying = false;
+let intervalId;
+
+/*const autoPlay = () => {
+
+};*/
+
+document.querySelector('.js-auto-play-button')
+  .addEventListener('click', () => autoPlay());
+
+document.body.addEventListener('keydown', event => {
+  if (event.key === 'a') {
+    autoPlay();
+  } else if(event.key === 'Backspace') {
+    renderResetConfirmation();
+  }
+});
+
+document.querySelector('.js-reset-score-button')
+  .addEventListener('click', () => renderResetConfirmation());
+
+function resetScore() {
+  score.wins = 0; 
+  score.losses = 0; 
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+}
+
+function renderResetConfirmation() {
+  const divElement = document.querySelector('.js-reset-confirmation');
+
+  divElement.innerHTML = 
+  `
+    <p>
+      Are you sure you want to reset the score?
+      <button class="js-confirm-reset-button">Yes</button>
+      <button class="js-deny-reset-button">No</button>
+    </p>
+  `;
+
+  document.querySelector('.js-confirm-reset-button')
+    .addEventListener('click', () => {
+      resetScore();
+      divElement.innerHTML = '';
+    });
+  
+  document.querySelector('.js-deny-reset-button')
+    .addEventListener('click', () => divElement.innerHTML = '');
+}
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+      intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+    document.querySelector('.js-auto-play-button').innerHTML = 'Stop Playing';
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    document.querySelector('.js-auto-play-button').innerHTML = 'Auto Play';
+  }
+}
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock'); // Can`t provide in eventListener as we instantly run function, we don`t provide function as param
+  });
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper'); // Can`t provide in eventListener as we instantly run function, we don`t provide function as param
+  });
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('scissors'); // Can`t provide in eventListener as we instantly run function, we don`t provide function as param
+  });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = '';
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.'
+    } else if (computerMove === 'paper') {
+      result = 'You win.';
+    } else {
+      result = 'Tie.';
+    }
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.'
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else {
+      result = 'You lose.';
+    }
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie.'
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else {
+      result = 'You win.';
+    }
+  }
+
+  if (result === 'You win.') {
+    score.wins++;
+  } else if (result === 'You lose.') {
+    score.losses++;
+  } else if (result === 'Tie.') {
+    score.ties++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  updateScoreElement();
+  document.querySelector('.js-result')
+    .innerHTML = result;
+  document.querySelector('.js-moves')
+    .innerHTML = `
+      You <img src="img/${playerMove}-emoji.png" class="move-icon">
+      <img src="img/${computerMove}-emoji.png" class="move-icon">
+      Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector('.js-score')
+  .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+  
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+  computerMove = 'rock';
+  } else if (randomNumber >= 1/3 && randomNumber  < 2/3) {
+  computerMove = 'paper';
+  } else {
+  computerMove = 'scissors';
+  }
+
+  return computerMove;
+}
