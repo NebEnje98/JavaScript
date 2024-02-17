fcc21dfc5145f66012ba8182394ba8e19b6f646f
Nebojsa Enjev <enjev.nebojsa@gmail.com>
2024-02-17 20:47:26 +0100

    Added winner alert functionality and reseting of the game.

--- SmallProjects/TicTacToe/scripts/main.js
+++ SmallProjects/TicTacToe/scripts/main.js
@@ -1,6 +1,6 @@
 let playerTurn = 0;
-const isPlayable = [];
-const result = [];
+let isPlayable = [];
+let result = [];
 
 for (let i = 0; i < 9; i++) {
   isPlayable.push(true);
@@ -62,32 +62,59 @@ function playTurn(position) {
     document.querySelector(`.js-play-field${position+1}`).innerHTML = 'O';
     playerTurn = 0;
     isPlayable[position] = false;
-    result [position] = 'O';
+    result[position] = 'O';
   }
 
-  checkWin();
+  setTimeout(() => {
+    checkWin();
+  }, 50);
 }
 
 function checkWin() {
+
+  let winnerText = '';
+  let isWon = false;
 
   for (let i = 0; i < 3; i++) {
     let row = i*3;
     
     if (result[row] === result[row+1] & result[row] === result[row+2] & result[row] !== '') {
-      console.log(`Winner: ${result[row]}`);
+      winnerText = `Winner: ${result[row]}`;
+      isWon = true;
     }
 
     if (result[i] === result[i+3] & result[i] === result[i+6] & result[i] !== '') {
-      console.log(`Winner: ${result[i]}`);
+      winnerText = `Winner: ${result[i]}`;
+      isWon = true;
     }
   }
 
   if (result[0] === result[4] & result[0] === result[8] & result[0] !== '') {
-    console.log(`Winner: ${result[0]}`);
+    winnerText = `Winner: ${result[0]}`;
+    isWon = true;
   }
 
   if (result[2] === result[4] & result[2] === result[6] & result[2] !== '') {
-    console.log(`Winner: ${result[2]}`);
+    winnerText = `Winner: ${result[2]}`;
+    isWon = true;
   }
 
+  if (isWon) {
+    alert(winnerText);
+    resetPlay();
+    winnerText = '';
+    isWon = false;
+  }
+}
+
+function resetPlay() {
+  playerTurn = 0;
+  isPlayable = [];
+  result = [];
+
+  for (let i = 0; i < 9; i++) {
+    isPlayable.push(true);
+    result.push('');
+    document.querySelector(`.js-play-field${i+1}`).innerHTML = '';
+  }
 }
